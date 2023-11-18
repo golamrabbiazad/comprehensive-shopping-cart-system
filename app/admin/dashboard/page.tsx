e08e6688ed17bc80d7bb622a0be1e3b0c3751c3a
Golamrabbi Azad <golamrabbiazad@gmail.com>
2023-11-18 23:38:49 +0600

    chore: partially info added to admin dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,26 @@
-import { currentUser } from "@clerk/nextjs";
+import prisma from "@/prisma/client";
 
 export default async function AdminDashboard() {
-  return <div>Admin Dashboard</div>;
+  const orders = await prisma.order.findMany({
+    include: {
+      items: true,
+    },
+  });
+
+  if (!orders) {
+    return <div>Orders not created yet.</div>;
+  }
+
+  return (
+    <div className="mx-auto flex flex-col py-12 justify-start items-center min-h-screen overflow-x-auto">
+      {orders.map((order) => (
+        <ul key={order.id}>
+          <li>Order Status</li>
+          <li>
+            {order.status} {order.items.length} items
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
